dc1dc010bbd7e7dfd8ab525c97a80205cfaa1f6d
“Michelle <MNguyen000@sea.yearup.org>
2021-08-04 10:51:18 -0700

    Update

--- index.php
+++ index.php
@@ -20,7 +20,15 @@ $(document).ready(function() {
 function loadAJAX(cat)
 {
 	//AJAX connection will go here
-    alert('cat is: ' + cat);
+    //alert('cat is: ' + cat);
+
+		$.ajax({
+		type: "GET",
+		dataType: "json",
+		url: "api.php?cat=" + cat,
+		success: bondJSON
+		});
+
 }
     
 function toConsole(data)
@@ -29,7 +37,7 @@ function toConsole(data)
 }
 
 function bondJSON(data){
-//JSON processing data goes here
+	console.log(data);
 }
 
 </script>
